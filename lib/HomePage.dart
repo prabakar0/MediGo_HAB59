fedc4980d0661c5ca1e94e0d436d0cfa8df81ffa
ADITI <aditi.ravichandran22@gmail.com>
2020-08-09 23:15:45 +0530

    d1 commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:clay_containers/clay_containers.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    //double width=MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Color(0xFFF2F2F2),
+      //backgroundColor: Colors.white.withOpacity(0.9),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25),),
+                gradient: LinearGradient(
+                  colors: [colour1,colour2],
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  //stops: [0.0,0.9],
+                  tileMode: TileMode.clamp,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15,bottom: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    SizedBox(height: 85,),
+                    Text('Welcome Back,',style: TextStyle(fontSize: 18,color: Colors.white),),
+                    Text('Samsritha',style: TextStyle(fontSize: 40,color: Colors.white,fontWeight: FontWeight.bold),),
+                    //SizedBox(height: 10,),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 17,),
+            Padding(
+              padding: const EdgeInsets.only(left:20.0),
+              child: Text('What do you need?',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15,left: 15,right: 15,top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: ClayContainer(
+                      height: 100,
+                      //width: width*0.4,
+                      //color: Color(0xFFF2F2F2),
+                      depth: 19,
+                      spread: 8,
+                      borderRadius: 15,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 15,),
+                          Image.asset('images/syringe.png',height: 60,width: 60,),
+                          Text('Pharmaceutical'),
+                        ],
+                      ),
+                      //curveType: CurveType.convex,
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Expanded(
+                    child: ClayContainer(
+                      height: 100,
+                      //width: width*0.4,
+                      depth: 19,
+                      spread: 8,
+                      borderRadius: 15,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 15,),
+                          Image.asset('images/medical-assistance.png',height: 60,width: 60,),
+                          Text('Mental Assistance'),
+                        ],
+                      ),
+                      //curveType: CurveType.convex,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 15,left: 15,right: 15,top: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: ClayContainer(
+                      height: 100,
+                      //width: width*0.4,
+                      //color: Color(0xFFF2F2F2),
+                      depth: 19,
+                      spread: 8,
+                      borderRadius: 15,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 15,),
+                          Image.asset('images/medical-box.png',height: 60,width: 60,),
+                          Text('Emergency practices'),
+                        ],
+                      ),
+                      //curveType: CurveType.convex,
+                    ),
+                  ),
+                 SizedBox(width: 17,),
+                  Expanded(
+                    child: ClayContainer(
+                      height: 100,
+                      //width: width*0.4,
+                      depth: 19,
+                      spread: 8,
+                      borderRadius: 15,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 15,),
+                          Image.asset('images/personal-information.png',height: 60,width: 60,),
+                          Text('Medical ID'),
+                        ],
+                      ),
+                      //curveType: CurveType.convex,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15,),
+            Padding(
+              padding: const EdgeInsets.only(left:20.0),
+              child: Text('Health Articles',style: TextStyle(fontSize: 19,fontWeight: FontWeight.bold),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ClayContainer(
+                height: 150,
+                depth: 19,
+                spread: 8,
+                borderRadius: 15,
+                //curveType: CurveType.convex,
+              ),
+            ),
+//            Padding(
+//              padding: const EdgeInsets.all(15.0),
+//              child: ClayContainer(
+//                height: 150,
+//                depth: 12,
+//                spread: 8,
+//                borderRadius: 15,
+//                //curveType: CurveType.convex,
+//              ),
+//            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
